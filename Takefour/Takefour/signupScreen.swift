10cb732bd95033638f6beaf56921f2c6ef516847
jonahlindau <59710171+jonahlindau@users.noreply.github.com>
2023-05-11 16:27:25 -0500

    Add files via upload

---
+++ Takefour/Takefour/signupScreen.swift
@@ -0,0 +1,67 @@
+//
+//  signupScreen.swift
+//  Takefour
+//
+//  Created by Jonah Lindau on 5/9/23.
+//
+
+import UIKit
+import Firebase
+
+class signupScreen: UIViewController {
+
+
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        overrideUserInterfaceStyle = .light
+        
+        super.viewDidLoad()
+        self.hideKeyboardWhenTappedAround()
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+       let nextTag = textField.tag + 1
+       // Try to find next responder
+       let nextResponder = textField.superview?.viewWithTag(nextTag) as UIResponder?
+
+       if nextResponder != nil {
+           // Found next responder, so set it
+           nextResponder?.becomeFirstResponder()
+       } else {
+           // Not found, so remove keyboard
+           textField.resignFirstResponder()
+       }
+
+       return false
+   }
+    
+    
+    @IBAction func signupClicked(_ sender: Any) {
+        guard let email = emailTextField.text else { return }
+        guard let password = passwordTextField.text else { return }
+        
+        Auth.auth().createUser(withEmail: email, password: password) { firebaseResult, error in
+            if let e = error {
+                print("error")
+            }
+            else {
+                // Go to our home screen
+                self.performSegue(withIdentifier: "accountCreated", sender: self)
+            }
+        }
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
