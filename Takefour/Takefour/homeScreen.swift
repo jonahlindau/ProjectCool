a16eaf6038a7074893d51119a092fc0b933c574c
jonahlindau <59710171+jonahlindau@users.noreply.github.com>
2023-06-28 12:26:41 -0500

    6/28/23 upload

---
+++ Takefour/Takefour/homeScreen.swift
@@ -0,0 +1,29 @@
+//
+//  homeScreen.swift
+//  Takefour
+//
+//  Created by Jonah Lindau on 5/10/23.
+//
+
+import UIKit
+
+class homeScreen: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        overrideUserInterfaceStyle = .light
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
